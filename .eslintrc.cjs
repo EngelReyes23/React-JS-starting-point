08b57e6e3f21b5dca4c36385bceece023712416d
EngelReyes23 <engelreyes23@gmail.com>
2023-11-01 23:08:03 -0600

    Add lint-staged and husky for pre-commit hooks and formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
+    './node_modules/standard/eslintrc.json'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
 }
